daf5a5f0698b2eeb07ab318bf21897be5ced13e5
DmKeller <dmit.keller@gmail.com>
2023-04-02 11:50:43 +0300

    Initial Commit

---
+++ Magic 8 Ball_iOS16.2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Magic 8 Ball_iOS16.2
+//
+//  Created by Келлер Дмитрий on 02.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
